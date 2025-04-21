bebbbf74372fcba1d9976363817713b70f593bf0
Ujjwal <Ujjwal.108694@stu.upes.ac.in>
2025-04-21 21:08:19 +0530

    files added

---
+++ src/main/java/com/example/springboot_cicd_github/SpringbootCicdGithubApplication.java
@@ -0,0 +1,21 @@
+package com.example.springboot_cicd_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootCicdGithubApplication {
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "welcome to my project!!!";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCicdGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_cicd_github/SpringbootCicdGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_cicd_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicdGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
